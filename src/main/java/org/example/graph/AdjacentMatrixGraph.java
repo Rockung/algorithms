e0c185dc7329281cfe0403e78bcd332a23863c17
Rockung <rockung@lesiji.top>
2019-10-02 09:47:53 +0800

    breadth-first search for AM graph

--- src/main/java/org/example/graph/AdjacentMatrixGraph.java
+++ src/main/java/org/example/graph/AdjacentMatrixGraph.java
@@ -14,7 +14,8 @@ public class AdjacentMatrixGraph {
 
     public static void main(String[] args) {
         // test01();
-        test02();
+        // test02();
+        test03();
     }
 
     /**
@@ -82,12 +83,12 @@ public class AdjacentMatrixGraph {
      * @param v the start vertex
      * @return the list of path
      */
-    public List<Integer> depthFirstSearch(String v) {
+    public List<Edge> depthFirstSearch(String v) {
         int index = indexOf(v);
         if (index == -1) return new ArrayList<>();
 
         // prepare for recursive call
-        ArrayList<Integer> path = new ArrayList<>();
+        ArrayList<Edge> path = new ArrayList<>();
         boolean[] visited = new boolean[vertexes.length];
 
         // do depth-first searching
@@ -97,26 +98,58 @@ public class AdjacentMatrixGraph {
     }
 
     /**
-     * Add **start** to the path, mark it as visited and find the next one recursively
+     * Do depth-first search from **start**
+     *   1. mark it as visited
+     *   2. add the first edge which end is not yet visited to the path
+     *   3. recursively forward
      *
      * @param start
      * @param visited
      * @param path
      */
-    private void doDepthFirstSearch(int start, boolean[] visited, ArrayList<Integer> path) {
+    private void doDepthFirstSearch(int start, boolean[] visited, ArrayList<Edge> path) {
         visited[start] = true;
-        path.add(start);
 
         for (int i = 0; i < edgeMatrix[0].length; i++) {
             if (edgeMatrix[start][i] == 1 && !visited[i]) {
+                path.add(new Edge(start, i));
                 doDepthFirstSearch(i, visited, path);
-                break;
             }
         }
     }
 
-    public void breathFirstSearch() {
+    /**
+     * Breadth-first search
+     *   use FIFO queue to visit vertexes
+     *   mark the visited vertexes
+     *
+     * @param v the start vertex
+     * @return
+     */
+    public List<Edge> breadthFirstSearch(String v) {
+        int start = indexOf(v);
+        if (start == -1) return new ArrayList<>();
 
+        ArrayList<Edge> path = new ArrayList<>();
+        ArrayList<Integer> queue = new ArrayList<>();
+        boolean[] visited = new boolean[vertexes.length];
+
+        queue.add(start);
+        while(!queue.isEmpty()) {
+            start = queue.remove(0);
+            visited[start] = true;
+
+            for (int i = 0; i < edgeMatrix[0].length; i++) {
+                if (edgeMatrix[start][i] == 1 && !visited[i]) { // not visited yet
+                    visited[i] = true;
+                    path.add(new Edge(start, i));
+
+                    queue.add(i); // add to the queue for the next loop
+                }
+            }
+        }
+
+        return path;
     }
 
     public void printGraphMatrix() {
@@ -163,6 +196,17 @@ public class AdjacentMatrixGraph {
         return this.vertexes[i];
     }
 
+
+    public static class Edge {
+        int v1;
+        int v2;
+
+        public Edge(int v1, int v2) {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+
     /**
      * Create a graph for test
      *
@@ -183,6 +227,27 @@ public class AdjacentMatrixGraph {
         return graph;
     }
 
+    private static AdjacentMatrixGraph createGraph02() {
+        AdjacentMatrixGraph graph = new AdjacentMatrixGraph(new String[] {
+                "1", "2", "3", "4", "5", "6", "7", "8"
+        });
+
+        graph.addEdge("1", "2");
+        graph.addEdge("1", "3");
+
+        graph.addEdge("2", "4");
+        graph.addEdge("2", "5");
+        graph.addEdge("2", "6");
+
+        graph.addEdge("3", "7");
+        graph.addEdge("3", "8");
+
+        graph.addEdge("4", "5");
+        graph.addEdge("7", "8");
+
+        return graph;
+    }
+
     private static void test01() {
         AdjacentMatrixGraph graph = createGraph01();
         graph.printGraphMatrix();
@@ -195,11 +260,22 @@ public class AdjacentMatrixGraph {
     }
 
     private static void test02() {
-        AdjacentMatrixGraph graph = createGraph01();
-        List<Integer> path = graph.depthFirstSearch("a");
+        AdjacentMatrixGraph graph = createGraph02();
+        List<Edge> path = graph.depthFirstSearch("1");
 
-        for (int i: path) {
-            System.out.print(graph.indexOf(i) + "->");
+        for (Edge e: path) {
+            System.out.print(graph.indexOf(e.v1) + "-" + graph.indexOf(e.v2) + " ");
+        }
+        System.out.println();
+
+    }
+
+    private static void test03() {
+        AdjacentMatrixGraph graph = createGraph02();
+        List<Edge> path = graph.breadthFirstSearch("1");
+
+        for (Edge e: path) {
+            System.out.print(graph.indexOf(e.v1) + "-" + graph.indexOf(e.v2) + " ");
         }
         System.out.println();
 
